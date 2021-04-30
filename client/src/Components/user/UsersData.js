e17f7b83a560500636c344c5405549ae9510c756
rrcg-chetan <80247776+rrcg-chetan@users.noreply.github.com>
2021-04-30 15:16:33 +0530

    fields added and shuffled

--- client/src/Components/user/UsersData.js
+++ client/src/Components/user/UsersData.js
@@ -16,23 +16,18 @@ const UsersData = props => {
 
     const columns = [
         {
-          name: 'Hospital ID',
-          selector: 'id',
-          sortable: true,
+            name: 'Id',
+            selector: 'code',
+            sortable: true
         },
         {
-          name: 'Generated Id',
-          selector: 'code',
-          sortable: true
+            name: 'Patient Name',
+            selector: 'patient_name',
+            sortable: false
         },
         {
-          name: 'Date of Birth',
-          selector: 'patients_dob',
-          sortable: false
-        },
-        {
-            name: 'Status',
-            selector: 'status',
+            name: 'Date of Birth',
+            selector: 'patients_dob',
             sortable: false
         },
         {
@@ -53,7 +48,7 @@ const UsersData = props => {
         {
             name: "Options",
             sortable: false,
-            cell: d => <div><a href={'demography/share/'+d.code}><svg data-v-9a6e255c="" xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" id="invoice-row-4996-send-icon" className="cursor-pointer feather feather-send"><line data-v-9a6e255c="" x1="22" y1="2" x2="11" y2="13"></line><polygon data-v-9a6e255c="" points="22 2 15 22 11 13 2 9 22 2"></polygon></svg></a><span style={{ color: "#7367f0"}} id={d.code} onClick={(e) => { if (window.confirm('Are you sure you wish to delete this item?')) deletePatientRecord(e) } }><svg data-v-9a6e255c="" xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="cursor-pointer mx-1 feather feather-trash"><polyline data-v-9a6e255c="" points="3 6 5 6 21 6"></polyline><path data-v-9a6e255c="" d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></span><a href={'demography/edit/'+d.code}><svg data-v-9a6e255c="" xmlns="http://www.w3.org/2000/svg" width="14px" height="14px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="feather feather-edit"><path data-v-9a6e255c="" d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path data-v-9a6e255c="" d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></a>
+            cell: d => <div><span style={{ color: "#7367f0"}} id={d.code} onClick={(e) => { if (window.confirm('Are you sure you wish to delete this item?')) deletePatientRecord(e) } }><svg data-v-9a6e255c="" xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="cursor-pointer mx-1 feather feather-trash"><polyline data-v-9a6e255c="" points="3 6 5 6 21 6"></polyline><path data-v-9a6e255c="" d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></span><a href={'demography/edit/'+d.code}><svg data-v-9a6e255c="" xmlns="http://www.w3.org/2000/svg" width="14px" height="14px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="feather feather-edit"><path data-v-9a6e255c="" d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path data-v-9a6e255c="" d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></a>
             </div>
         }
       ];
